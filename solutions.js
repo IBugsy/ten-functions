5c84900c80cd668aa0db24fe77a0a277058ebe5c
Steven Bugenske <sbugenske@gmail.com>
2023-11-15 15:50:22 -0600

    first ten-functions assessment

--- solutions.js
+++ solutions.js
@@ -21,3 +21,79 @@
  * concat
  */
 
+//Question 1 isTrue
+
+function isTrue(input){
+    return input === true;
+}
+
+//Question 2 isFalse
+
+function isFalse(input) {
+    return input === false;
+}
+
+//Question 3 not
+
+function not(input){
+    return !input;
+}
+
+//Question 4 addOne
+
+function addOne(input){
+    input =Number(input)
+    return input + 1;
+}
+
+//Question 5 isEven
+
+function isEven(input){
+        if(typeof input === 'boolean'){
+            return false;
+        }
+        return input % 2 === 0;
+}
+
+//Question 6 isIdentical
+
+function isIdentical(a, b){
+    return (a === b) || (a !== b);
+}
+
+//Question 7 isEqual
+function isEqual(){
+
+}
+
+//Question 8 or
+function or(){
+
+}
+
+//Question 9 and
+function and(){
+
+}
+//Question 10 concat
+
+function concat(a, b) {
+    if (
+        typeof a === 'string'
+        && typeof b === 'string'
+    ) {
+        return a + b;
+    }
+    if (
+        typeof a === 'number'
+        && typeof b === 'number'
+    ) {
+
+            return String(a) + String(b);
+    }
+    if (
+        typeof a === 'boolean'
+        && typeof b === 'boolean'
+    ) {
+}
+}
